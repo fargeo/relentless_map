ab10c46aca18d4241ac7187d8e471ac9e9470ae3
Rob Gaston <rgaston@fargeo.com>
2022-11-17 14:41:39 -0800

    updates map, adds UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import * as mapboxgl from "mapbox-gl";
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css';
 import settings from "./settings.json";
 import custom from "./custom-style.json";
 
@@ -16,6 +20,15 @@ async function init() {
     style.layers.push(...custom.layers);
     map.setStyle(style);
 
+    map.addControl(new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        bbox: [
+            -86.31092854091077, 38.255831759695326,
+            -78.77605534835592, 42.42310884088013
+        ]
+    }));
+
     map.addControl(new mapboxgl.NavigationControl());
 
     map.addControl(new mapboxgl.GeolocateControl({
@@ -25,6 +38,48 @@ async function init() {
         trackUserLocation: true,
         showUserHeading: true
     }));
+
+    document.querySelector('.expand-filters').addEventListener('click', () => {
+        const form = document.querySelector('#filter-form');
+        form.classList.toggle('expanded');
+    });
+
+    const updateFilters = () => {
+        let filter = null;
+        const dates = datefilter.selectedDates;
+        const vehicleID = vehicleFilter.value;
+        if (vehicleID) {
+            filter = ['==', ['get', 'vehicleid'], vehicleID]
+        }
+        if (dates.length === 2) {
+            const dateFilter = [
+                "all",
+                [">=", ['get', 'trip_day'], dates[0].toISOString()],
+                ["<=", ['get', 'trip_day'], dates[1].toISOString()]
+            ];
+            if (filter) {
+                dateFilter.push(filter);
+            }
+            filter = dateFilter;
+        }
+        map.setFilter('original', filter);
+    };
+
+    map.on('click', 'original', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.vehicleid)
+            .addTo(map);
+    });
+
+    const datefilter = flatpickr('#rangeDate', {
+        mode: 'range',
+        dateFormat: "Y-m-d",
+        onChange: updateFilters
+    });
+
+    const vehicleFilter = document.querySelector('#vehicleID');
+    vehicleFilter.addEventListener('keyup', updateFilters);
 }
 
 mapboxgl.accessToken = settings.accessToken;
